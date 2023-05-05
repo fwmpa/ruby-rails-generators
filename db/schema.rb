7d81b0ebe3fd1fe75a85019ac78cd0b0e792a2df
Fernando Wagner <fwmpa2@gmail.com>
2023-05-05 17:12:34 -0300

    Integrated friendly routes for blog posts
    Signed-off-by: Fernando Wagner <fwmpa2@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_504_092_645) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_200048) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'blogs', force: :cascade do |t|
-    t.string 'title'
-    t.text 'body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
-  create_table 'portfolios', force: :cascade do |t|
-    t.string 'title'
-    t.string 'subtitle'
-    t.text 'body'
-    t.text 'main_image'
-    t.text 'thumb_image'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
-  create_table 'skills', force: :cascade do |t|
-    t.string 'title'
-    t.integer 'percent_utilized'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
